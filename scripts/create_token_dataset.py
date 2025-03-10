9123f73fb3b44a71db6e83ca396bb6a072c1e51b
Haesung Jeon <seastar105@gmail.com>
2025-03-10 16:36:43 +0900

    modify tokenize script

--- scripts/create_token_dataset.py
+++ scripts/create_token_dataset.py
@@ -10,7 +10,7 @@ from concurrent.futures import ThreadPoolExecutor
 import librosa
 import ray
 import torch
-from huggingface_hub import HfFileSystem, hf_hub_download
+from huggingface_hub import HfApi, HfFileSystem, get_token, hf_hub_download
 from tqdm.auto import tqdm
 
 from src.wavtok.pretrained import load_large_v2
@@ -57,19 +57,46 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    fs = HfFileSystem()
+    token = get_token()
+    fs = HfFileSystem(token=token)
     tar_files = sorted(fs.glob("datasets/seastar105/aihub-542-webdataset/**/*.tar"))
 
     # Download checkpoint before parallel execution
     load_large_v2()
 
-    num_shards_per_job = 100
+    num_shards_per_job = 32
+    api = HfApi(token=token)
+
+    def download_file(tar_file):
+        return hf_hub_download(
+            repo_id=tar_file.repo_id, filename=tar_file.path_in_repo, local_dir="./cache", repo_type="dataset"
+        )
 
     for i in range(0, len(tar_files), num_shards_per_job):
+        shard_idx = i // num_shards_per_job
+        output_dir = f"{args.output_dir}/shard_{shard_idx:03d}"
+        # check if already output exists on remote repo
+        if len(fs.glob(f"datasets/seastar105/aihub-542-tokenized/{output_dir}/*.json")) > 0:
+            print(f"Skipping {output_dir} as it already exists")
+            continue
+
         shards = tar_files[i : i + num_shards_per_job]
+        local_paths = []
+        with ThreadPoolExecutor(max_workers=min(8, num_shards_per_job)) as executor:
+            for local_path in tqdm(executor.map(download_file, shards), desc="Downloading tar files..."):
+                local_paths.append(local_path)
         dataset = (
             ray.data.read_webdataset(shards, filesystem=fs)
             .map(AudioDecoder, concurrency=args.gpu_concurrency * 4)
             .map(WavTokActor, num_gpus=args.num_gpus, concurrency=args.gpu_concurrency)
         )
-        dataset.write_json(args.output_dir, force_ascii=False, min_rows_per_file=100000)
+
+        dataset.write_json(output_dir, force_ascii=False, min_rows_per_file=50000)
+        api.upload_folder(
+            repo_id="seastar105/aihub-542-tokenized",
+            path_in_repo=f"shard_{shard_idx:03d}",
+            repo_type="dataset",
+        )
+
+        for local_path in local_paths:
+            os.remove(local_path)
